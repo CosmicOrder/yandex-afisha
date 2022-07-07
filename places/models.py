b5b0b3ad43c61398305f75a63d195360e7492463
CosmicOrder <Matrixxturn@yandex.ru>
2022-07-08 00:12:06 +0300

    feat: add place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.forms import JSONField
 
-print('Hello, World!')
+
+class Place(models.Model):
+    title = models.CharField('Название компании', max_length=200)
+    imgs = models.ImageField('Картинка', blank=True)
+    description_short = models.TextField('Кратное описание')
+    description_long = models.TextField('Полное описание')
+    coordinates = JSONField('Координаты')
+
+    def __str__(self):
+        return self.title
