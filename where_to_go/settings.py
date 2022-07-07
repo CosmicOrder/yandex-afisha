daf5f3a456a2cef24ea852f4b7e1fabbf2359fde
CosmicOrder <Matrixxturn@yandex.ru>
2022-07-07 23:36:39 +0300

    feat: add frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -88,7 +88,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
