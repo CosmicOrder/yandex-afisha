454c6fe93b29c5f4c0724f62248370aa4c93e652
CosmicOrder <Matrixxturn@yandex.ru>
2022-07-08 22:07:11 +0300

    feat: add model for images

--- places/models.py
+++ places/models.py
@@ -4,10 +4,22 @@ from django.forms import JSONField
 
 class Place(models.Model):
     title = models.CharField('Название компании', max_length=200)
-    imgs = models.ImageField('Картинка', blank=True)
     description_short = models.TextField('Кратное описание')
     description_long = models.TextField('Полное описание')
     coordinates = JSONField('Координаты')
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        verbose_name='Картинка',
+        related_name='images',
+    )
+    picture = models.ImageField('Картинка')
+
+    def __str__(self):
+        return f'{self.pk} ' + self.place.title
