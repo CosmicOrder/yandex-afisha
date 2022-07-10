cae59f8d6921bd59145e181bbe2e178eb66d9c6c
CosmicOrder <Matrixxturn@yandex.ru>
2022-07-11 01:30:17 +0300

    feat: move more settings to env, add README.md

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -87,13 +87,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static/')
+STATIC_URL = env('STATIC_URL', '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, env('STATIC_ROOT'))
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = env('MEDIA_URL', '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, env('MEDIA_ROOT'))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
